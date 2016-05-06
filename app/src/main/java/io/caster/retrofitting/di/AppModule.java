58b28960446f971c144534b2f4f88fc468b77dc7
Donn Felker <donn@donnfelker.com>
2016-05-06 17:52:39 -0400

    Adding logging

--- app/src/main/java/io/caster/retrofitting/di/AppModule.java
+++ app/src/main/java/io/caster/retrofitting/di/AppModule.java
@@ -2,8 +2,11 @@ package io.caster.retrofitting.di;
 
 import dagger.Module;
 import dagger.Provides;
+import io.caster.retrofitting.BuildConfig;
 import io.caster.retrofitting.Constants;
 import io.caster.retrofitting.api.BookService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,10 +14,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class AppModule {
 
     @Provides
-    public Retrofit provideRetrofit() {
+    public OkHttpClient provideLoggingCapableHttpClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+        logging.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+
+        return new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+    }
+
+    @Provides
+    public Retrofit provideRetrofit(OkHttpClient okHttpClient) {
         return new Retrofit.Builder()
                 .baseUrl(Constants.API.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 
