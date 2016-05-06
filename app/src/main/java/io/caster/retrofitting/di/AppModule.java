786856faba4d978c2a249b848ae642b7b6fad7a8
Donn Felker <donn@donnfelker.com>
2016-05-06 15:51:04 -0400

    Removing old code'

--- app/src/main/java/io/caster/retrofitting/di/AppModule.java
+++ app/src/main/java/io/caster/retrofitting/di/AppModule.java
@@ -1,7 +1,5 @@
 package io.caster.retrofitting.di;
 
-import com.google.gson.Gson;
-
 import dagger.Module;
 import dagger.Provides;
 import io.caster.retrofitting.Constants;
@@ -11,12 +9,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class AppModule {
-
-//    @Provides @Singleton
-//    public Gson provideGson() {
-//        // Do fancy stuff with GSON here. :) (Custom date converters/etc)
-//        return new GsonBuilder().create();
-//    }
 
     @Provides
     public Retrofit provideRetrofit() {
